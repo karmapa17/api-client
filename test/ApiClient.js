0c249ac465164c4c37550239bba9e909c4f243f3
kmsheng <kmsh3ng@gmail.com>
2017-08-02 10:38:06 +0800

    add const API_URL

--- test/ApiClient.js
+++ test/ApiClient.js
@@ -1,9 +1,11 @@
 import test from 'ava';
 import ApiClient from './../src/ApiClient';
 
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
 test('get function should work properly', async (t) => {
 
   const apiClient = new ApiClient();
-  const result = await apiClient.get('https://api.dharma-treasure.org');
+  const result = await apiClient.get(`${API_URL}/posts`);
   t.truthy(result);
 });
